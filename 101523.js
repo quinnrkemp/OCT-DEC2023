d3439b97e145bb43ad4952c072ef5dfdfb4ff3c6
quinnrkemp <a467814@gmail.com>
2023-10-15 23:43:13 -0400

    coding chall for 101523

---
+++ 101523.js
@@ -0,0 +1,26 @@
+// CHALLENGE NAME-  Function 2 - squaring an argument
+
+// DESCRIPTION:
+
+// Now you have to write a function that takes an argument 
+// and returns the square of it.
+
+// // ***STARTER CODE***
+
+// // Write the "square"-function here 
+
+// //   ******TEST CASES*****
+
+// const { assert } = require('chai');
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(square(3), 9);
+//   });
+// });
+
+// ******MY ANSWER********
+
+function square (num) {
+    return num *num
+  }
