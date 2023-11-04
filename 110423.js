a8fa48fffed8155e69892fdd05f3a40bbd64e62b
quinnrkemp <a467814@gmail.com>
2023-11-04 18:01:25 -0400

    coding chall for 110423

---
+++ 110423.js
@@ -0,0 +1,28 @@
+// CHALLENGE NAME-  Basic variable assignment
+
+// DESCRIPTION:
+
+// This code should store "codewa.rs" as a variable called name 
+// but it's not working. Can you figure out why?
+
+// // ***STARTER CODE***
+
+// var a == "code";
+// var b == "wa.rs";
+// var name == a + b;
+
+// //   ******TEST CASES*****
+
+// const { assert } = require("chai")
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(name, "codewa.rs");
+//     })
+//   })
+
+// ******MY ANSWER********
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
