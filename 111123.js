e5993d3cef882bafe1ead9069dceceddcce17232
quinnrkemp <a467814@gmail.com>
2023-11-11 18:35:49 -0500

    coding chall for 111123

---
+++ 111123.js
@@ -0,0 +1,34 @@
+    // CHALLENGE NAME-  Grasshopper- Combine strings
+
+    // DESCRIPTION:
+
+//     Create a function named combineNames that accepts two parameters 
+//     (first and last name). The function should return the full name.
+
+//     Example:
+    
+//     combineNames('James', 'Stevens')
+//     returns:
+    
+//     'James Stevens'
+
+//     // ***STARTER CODE***
+
+//   // Create the combineNames function here  
+
+//     //   ******TEST CASES*****
+    
+//     const Test = require('@codewars/test-compat');
+
+//     describe("Tests", () => {
+//       it("test", () => {
+//     Test.assertEquals(combineNames('James', 'Stevens'), 'James Stevens')
+//       });
+//     });
+    
+    // ******MY ANSWER********
+
+    function combineNames (last, first){
+        return `${last} ${first}`    
+        
+      }
