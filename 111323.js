2b9ea0e77a93250b5214c601ba2076b40e10e846
quinnrkemp <a467814@gmail.com>
2023-11-13 14:47:15 -0500

    coding chall for 111323

---
+++ 111323.js
@@ -0,0 +1,39 @@
+    // CHALLENGE NAME-  Configure package json for a node application
+
+    // DESCRIPTION:
+
+//     Create a package.json configuration and assign it to the configuration 
+//     constant so it can be tested.
+
+//     To pass the kata you need only the least that a real package file would 
+//     require.
+    
+//     Notes
+//     the kata version validation doesn't handle all cases and isn't 100% 
+//     accurate, so don't try to be too fancy, stick to with the simple
+//     to simplify the license validation this kata only allows the following
+//     ISC
+//     MIT
+//     UNLICENSED
+//     SEE LICENSE IN filename
+    
+
+//     // ***STARTER CODE***
+
+//     const configuration = {
+//         "name": "your-pack-name",
+//         "description": ""
+//       };
+//       // Hint: try creating with `npm init` command
+//       // Help! -> https://docs.npmjs.com/files/package.json
+
+//     //   ******TEST CASES*****
+    
+// test cases are very lengthy...
+
+    // ******MY ANSWER********
+
+    const configuration = {
+        "name": "your-pack-name",
+        "version": "1.2.5"
+      };
