0874cd995a19a3ac99ab2dda97506dd90c32e947
quinnrkemp <a467814@gmail.com>
2023-11-17 10:12:35 -0500

    coding chall for 111723

---
+++ 111723.js
@@ -0,0 +1,91 @@
+    // CHALLENGE NAME-  Fundamentals: Return
+
+    // DESCRIPTION:
+
+    // Make multiple functions that will return the sum, difference, 
+    // modulus, product, quotient, and the exponent respectively.
+
+    // Please use the following function names:
+    
+    // addition = add
+    
+    // multiply = multiply
+    
+    // division = divide (both integer and float divisions are accepted)
+    
+    // modulus = mod
+    
+    // exponential = exponent
+    
+    // subtraction = subt
+    
+    // Note: All math operations will be: a (operation) b
+    
+    //     // ***STARTER CODE***
+
+    //     function add(a,b){
+    //         return 
+    //     }
+        
+    //     function divide(a,b){
+    //         return 
+    //     }
+        
+    //     function multiply(a,b){
+    //         return 
+    //     }
+        
+    //     function mod(a,b){
+    //         return 
+    //     }
+           
+    //     function exponent(a,b){
+    //         return 
+    //     }
+            
+    //     function subt(a,b){
+    //         return 
+    //     }   
+
+    // //   ******TEST CASES*****
+    
+    // const chai = require("chai");
+    // const assert = chai.assert;
+    // chai.config.truncateThreshold=0;
+    
+    // describe("Basic tests", () => {
+    //   it("Testing for fixed tests", () => {
+    //     assert.strictEqual(add(1,2),3)
+    //     assert.strictEqual(multiply(1,2),2)
+    //     assert.strictEqual(divide(2,1),2)
+    //     assert.strictEqual(mod(1,2),1)
+    //     assert.strictEqual(exponent(1,2),1)
+    //     assert.strictEqual(subt(1,2),-1)
+    //   })
+    // })
+
+    // ******MY ANSWER********
+
+    function add(a,b){
+        return a + b
+    }
+    
+    function divide(a,b){
+        return a/b
+    }
+    
+    function multiply(a,b){
+        return a*b
+    }
+    
+    function mod(a,b){
+        return a%b
+    }
+       
+    function exponent(a,b){
+        return a**b
+    }
+        
+    function subt(a,b){
+        return a-b
+    }
