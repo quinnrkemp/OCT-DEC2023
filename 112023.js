3dfa6a4c0029ef22b3e376353b2894b30d26e401
quinnrkemp <a467814@gmail.com>
2023-11-20 13:15:49 -0500

    coding chall for 112023

---
+++ 112023.js
@@ -0,0 +1,34 @@
+    // CHALLENGE NAME-  Template Strings
+
+    // DESCRIPTION:
+
+    // Task
+    // Your task is to return the correct string using the Template 
+    // String Feature.
+    // Input
+    // Two Strings, no validation is needed.
+    // Output
+    // You must output a string containing the two strings with the 
+    // word ```' are '```
+    // Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+
+    // // ***STARTER CODE***
+
+    // var TempleStrings = function(obj, feature) {
+    //     return //complete this
+    //   }  
+
+    // //   ******TEST CASES*****
+    
+    // describe("Tests", () => {
+    //     it("test", () => {
+    //   Test.assertEquals(TempleStrings("Animals","Good"), 'Animals are Good')
+      
+    //     });
+    //   });
+      
+    // ******MY ANSWER********
+
+    var TempleStrings = function(obj, feature) {
+        return `${obj} are ${feature}`
+      }
