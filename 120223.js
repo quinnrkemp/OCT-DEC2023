52bf93841a6a8283a75360fcaddbd77ae1a6db6c
quinnrkemp <a467814@gmail.com>
2023-12-02 21:47:44 -0500

    coding chall for 120223

---
+++ 120223.js
@@ -0,0 +1,33 @@
+    // CHALLENGE NAME-  Reverse List Order
+
+    // DESCRIPTION:
+
+    // In this kata you will create a function that takes in a 
+    // list and returns a list with the reverse order.
+
+    // Examples (Input -> Output)
+    // * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+    // * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+    // // ***STARTER CODE***
+
+    // function reverseList(list) {
+
+    // }  
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("reverseList", function(){
+    //   it("should reverse some sample arrays", function(){
+    //     Test.assertSimilar(reverseList([1,2,3,4]), [4,3,2,1]);
+    //     Test.assertSimilar(reverseList([3,1,5,4]), [4,5,1,3]);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function reverseList(list) {
+        return list.reverse()
+        }
