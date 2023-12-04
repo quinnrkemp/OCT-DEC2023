155f2ed359f230dbf015dfa58b911eca7aa25a5d
quinnrkemp <a467814@gmail.com>
2023-12-03 19:33:50 -0500

    coding chall for 120323

---
+++ 120323.js
@@ -0,0 +1,51 @@
+    // CHALLENGE NAME-  Grader
+
+    // DESCRIPTION:
+
+    // Create a function that takes a number as an argument and 
+    // returns a grade based on that number.
+
+    // Score	Grade
+    // Anything greater than 1 or less than 0.6	"F"
+    // 0.9 or greater	"A"
+    // 0.8 or greater	"B"
+    // 0.7 or greater	"C"
+    // 0.6 or greater	"D"
+
+    // // ***STARTER CODE***
+
+    // function grader(score) {
+  
+    // }  
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("Solution", function() {
+    //   it("should pass some sample tests", function() {
+    //     Test.assertEquals(grader(0.7), "C");
+    //     Test.assertEquals(grader(0.9), "A");
+    //     Test.assertEquals(grader(0.6), "D");
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function grader(score) {
+        if (score>=0.9 && score<=1){
+          return "A"
+        }  
+         else if (score>=0.8 && score<0.9){
+          return "B"
+        }  
+        else if (score>=0.7 && score<0.8){
+          return "C"
+        }  
+          else if (score>=0.6 && score<0.7){
+          return "D"
+        }  
+          else {
+            return "F"
+          }
+        }
