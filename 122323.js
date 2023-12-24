63446b67e5662b3b5cc8c1c868785171c72343e0
quinnrkemp <a467814@gmail.com>
2023-12-23 22:21:21 -0500

    coding chall for 122323

---
+++ 122323.js
@@ -0,0 +1,29 @@
+// CHALLENGE NAME-  Parse Float  (#2)
+
+// DESCRIPTION:
+
+// Write function parseF which takes an input and 
+// returns a number or null if conversion is not possible. 
+// The input can be one of many different types so be aware.
+
+// // ***STARTER CODE***
+
+// function parseF(s) {
+//   return null;
+// }
+
+// //   ******TEST CASES*****
+
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(parseF("1"), 1.0);
+//   });
+// }); 
+
+// ******MY ANSWER********
+
+function parseF(s) {
+    return isNaN(parseFloat(s)) ? null : parseFloat(s);
+  }
